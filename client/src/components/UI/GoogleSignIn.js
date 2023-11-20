466cd892214c20bfc86ba16f4a5e8b934a08121c
ccousill <chriscous00@gmail.com>
2023-11-19 21:23:59 -0500

    styled google sign in

--- client/src/components/UI/GoogleSignIn.js
+++ client/src/components/UI/GoogleSignIn.js
@@ -3,6 +3,7 @@ import {signInWithGoogle} from "../../firebase/firebase";
 import { useDispatch } from "react-redux";
 import { userActions } from "../../store/user-slice";
 import { useSubmit } from "react-router-dom";
+import GoogleButton from 'react-google-button'
 import * as userService from "../../services/user"
 function GoogleSignIn() {
   const dispatch = useDispatch();
@@ -24,14 +25,10 @@ function GoogleSignIn() {
   };
 
   return (
-    <div className="items-center w-1/2 mx-auto">
-      <button
-        className="py-2 w-full rounded-3xl text-white bg-red-600 my-5 hover:bg-red-500"
-        onClick={handleGoogleSignIn}
-      >
-        Sign in With Google
-      </button>
-
+    <div className="w-1/2 mx-auto flex justify-center items-center">
+      <div>
+      <GoogleButton onClick={handleGoogleSignIn}/>
+      </div>
     </div>
   );
 }
